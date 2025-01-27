be478d7b79e2785858f1277b1722ac63c05fc392
wanzofc <berlianawan498@gmail.com>
2025-01-27 14:50:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,91 +1,36 @@
 const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require("fs");
-const FormData = require('form-data');
-const crypto = require("crypto");
-const { pinterest, igdl, pinterest2, remini, mediafire, tiktok } = require('./downloader');
+const multer = require('multer');
+const fs = require('fs');
+const { ImageUploadService } = require('node-upload-images');
 const app = express();
-const PORT = 8080;
+const port = 8080;
 
+const upload = multer({ dest: 'uploads/' }); // Folder temporary untuk menyimpan file
 
-app.use(express.json({limit: '10mb'}))
-// app.use(express.static('public')) // Hapus baris ini
-
-//Endpoint for tiktok
-app.post('/tiktok', async (req, res) => {
-   try {
-     const { text } = req.body;
-      const result = await tiktok(text);
-     res.json(result);
-   } catch (error) {
-      res.status(500).json({ message: error.message });
-   }
-});
-
-// Endpoint for Pinterest
-app.get('/pinterest', async (req, res) => {
-  try {
-    const { query } = req.query;
-    const results = await pinterest(query);
-    res.json(results);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-// Endpoint for Pinterest V2
-app.get('/pinterest2', async (req, res) => {
-  try {
-    const { query } = req.query;
-    const results = await pinterest2(query);
-    res.json(results);
-  } catch (error) {
-     res.status(500).json({ message: error.message });
-  }
-});
-
-// Endpoint for mediafire
-app.get('/mediafire', async (req, res) => {
-   try {
-     const { query } = req.query;
-     const results = await mediafire(query);
-     res.json(results);
-  } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-//endpoint for igdl
-app.get('/igdl', async (req, res) => {
-  try {
-      const { query } = req.query;
-      const results = await igdl(query);
-      res.json(results);
-  } catch (error) {
-       res.status(500).json({ message: error.message });
-    }
-});
-//Endpoint Remini
-app.post('/remini', async (req, res) => {
+app.post('/upload', upload.single('image'), async (req, res) => {
     try {
-        const { image } = req.body;
-        const buffer = Buffer.from(image.split(",")[1], "base64");
-        const enhancedImage = await remini(buffer);
-        res.set('Content-Type', 'image/jpeg');
-        res.send(enhancedImage);
+        if (!req.file) {
+            return res.status(400).send('No image file provided.');
+        }
+    
+        const mime = req.file.mimetype;
+         if (!/image/.test(mime)) {
+           return res.status(400).send('File is not an image')
+        }
+        
+        const service = new ImageUploadService('pixhost.to');
+        const { directLink } = await service.uploadFromBinary(fs.readFileSync(req.file.path), 'wanzofc.jpg');
+        
+        fs.unlinkSync(req.file.path)
+
+        res.json({ url: directLink.toString() });
+
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        console.error('Error during upload:', error);
+       res.status(500).send('Internal Server Error'+ error.message);
     }
 });
 
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
