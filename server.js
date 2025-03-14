236f63a7883db8fe6824248b9b0a1e4dba1fd5d0
wulandari-tech <zanssxploit@gmail.com>
2025-03-14 22:33:14 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const security = require('./security'); // Pastikan file ini ada!
-
 const app = express();
 const server = http.createServer(app);
 
